a2161c6c95bfa4f468441364f5f8f97a9d9ea8b1
hapra123 <hardikprabhu123@gmail.com>
2025-01-21 23:18:43 +0530

    Test Deployment1 changes include ui refinesment with new html css frontend

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 # Flask app
 flask_app = Flask(__name__)
 
-# Flask route to render the UI
-@flask_app.route("/", methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
+# Route to display Hello World
+@flask_app.route("/")
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     flask_app.run(host="0.0.0.0", port=80)
