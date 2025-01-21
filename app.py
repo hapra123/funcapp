f1ca6fe0155bc62e9140dd31bbc10a780e708460
hapra123 <hardikprabhu123@gmail.com>
2025-01-21 23:24:54 +0530

    Test Deployment1 changes include ui refinesment with new html css frontend

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
-# Flask app
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-# Route to display Hello World
-@flask_app.route("/")
+@app.route("/")
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", port=80)
+    app.run()
